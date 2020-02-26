b9d479d87fcf1bf78f7ab52ed350d46461e6a413
Maxwell Baird <bairdm71@gmail.com>
2020-02-26 10:39:07 -0700

    added and tested vendor sells item

--- test/market_test.rb
+++ test/market_test.rb
@@ -9,17 +9,17 @@ class MarketTest < Minitest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @vendor1.stock(item1, 35)
-    @vendor1.stock(item2, 7)
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
-    @vendor2.stock(item4, 50)
-    @vendor2.stock(item3, 25)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
     @vendor3 = Vendor.new("Palisade Peach Shack")
-    @vendor3.stock(item1, 65)
+    @vendor3.stock(@item1, 65)
 
   end
 
@@ -45,5 +45,12 @@ class MarketTest < Minitest::Test
     @market.add_vendor(@vendor3)
     expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
     assert_equal expected, @market.vendor_names
+  end
+
+  def test_it_can_find_vendors_that_sell_item
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
   end
 end
