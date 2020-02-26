4c7e449d9165ec6ee72269438004e119ca718986
Maxwell Baird <bairdm71@gmail.com>
2020-02-26 10:21:29 -0700

    added test to check if invetory updated

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -26,4 +26,16 @@ class VendorTest<Minitest::Test
     vendor.stock(item1, 25)
     assert_equal 55, vendor.check_stock(item1)
   end
+
+  def test_it_can_update_invetory
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock(item1, 30)
+    expected1 = {item1 => 30}
+    assert_equal expected1, vendor.inventory
+    expected2 = {item1 => 30, item2 => 15}
+    vendor.stock(item2, 15)
+    assert_equal expected2, vendor.inventory
+  end
 end
